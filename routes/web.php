dcc1a2b367e605c889c0eb5375ee3a70a894c6a6
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-05-21 23:51:16 +0200

    Added more pages

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return view('home');
-});
+})->name('home');
+
+Route::get('/about', function(){
+    return view('about');
+})->name('about');
+
+Route::get('/news', function(){
+    return view('news');
+})->name('news');
